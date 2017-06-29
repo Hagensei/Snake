7795b9071d9cdc6bbec7a04f0552d9a80c833528
Lev_Home <lev_vs@mail.ru>
2017-06-29 22:07:05 +0900

    Функция Drow

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,7 +10,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '#';
+
+            Draw(x1, y1, sym1);
+
+        }
+
+        static void Draw (int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
